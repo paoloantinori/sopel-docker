6f11bdc0a7e01f9cb00fe5510ee8b3024bad68f7
Paolo Antinori <pantinor@redhat.com>
2017-05-02 13:45:55 +0200

    Added hack for RH Ipaas DNS issues

--- scripts/modules/jira.py
+++ scripts/modules/jira.py
@@ -5,23 +5,24 @@ from sopel.module import commands, NOLIMIT, example, rule
 from sopel import web, tools
 from sopel.formatting import colors, color, bold, underline
 
-if sys.version_info.major < 3:
-  from urllib2 import HTTPError
-  from urlparse import urlparse
-else:
-  from urllib.request import HTTPError
-  from urllib.parse import urlparse
-
 # https://issues.jboss.org/rest/api/2/issue/ENTESB-6449
-jboss_org_base_url = "https://issues.jboss.org"
+jboss_org_base_url_dns = "https://issues.jboss.org"
+
+# hack for rh ipaas
+headers = {'Host': 'issues.jboss.org'}
+jboss_org_base_url = "https://209.132.182.82"
+# end hack
+
 jboss_org_rest = jboss_org_base_url + "/rest/api/2/issue/"
 jboss_org_case = jboss_org_base_url + "/browse/"
 
+
 def query_jira(jira_id):
-  response = requests.get(jboss_org_rest + jira_id)
+  url = jboss_org_rest + jira_id
+  response = requests.get(url, headers=headers, verify=False)
   response = response.json()
   if "fields" in response:
-    return "[{0}] {1} - {2}".format(jira_id, response["fields"]["summary"], color(jboss_org_case + jira_id , colors.GREY))
+    return "[{0}] {1} - {2}".format(jira_id, response["fields"]["summary"], color(jboss_org_case.replace(jboss_org_base_url,jboss_org_base_url_dns) , colors.GREY))
   else:
     return "Sorry but I couldn't fetch the URL"
 
