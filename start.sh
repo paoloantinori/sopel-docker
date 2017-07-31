2679c1594e3d9f1295f00711c3571baa38d1d941
Paolo Antinori <pantinor@redhat.com>
2017-07-31 12:01:09 +0200

    Fighting with env vars

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 printenv
 set -x
 echo "Applying variables to config template"
-envsubst < ${PWD}/.sopel/default.cfg.tpl > ${PWD}/.sopel/default.cfg
+envsubst < $PWD/.sopel/default.cfg.tpl > $PWD/.sopel/default.cfg
 exec sopel
