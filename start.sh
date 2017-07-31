33e0f2a595f1371f00c036a192777d43ecf65139
Paolo Antinori <pantinor@redhat.com>
2017-07-31 12:04:36 +0200

    Fighting with env vars

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 printenv
 set -x
-echo "Applying variables to config template"
+echo "========== Applying variables to config template"
 envsubst < $PWD/.sopel/default.cfg.tpl > $PWD/.sopel/default.cfg
+echo "========== Applied configuration"
+cat $PWD/.sopel/default.cfg
+echo "Current User"
+whomi
 exec sopel
